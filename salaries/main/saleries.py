3f5816dab4b6118b4b8ffebcf8e3aa6d8781659e
Danny Chen <37634165+zhuodannychen@users.noreply.github.com>
2019-01-04 05:39:12 +0000

    Update saleries.py

--- salaries/main/saleries.py
+++ salaries/main/saleries.py
@@ -2,39 +2,32 @@ import torch
 import torch.nn as nn
 import pandas as pd 
 import numpy as np
-import matplotlib.pyplot as plt
 from torch.autograd import Variable
 
 dataset = pd.read_csv('./data/saleries_data/salaries.csv')
 
 x_temp = dataset.iloc[:, :-1].values
-y_temp = dataset.iloc[:, 1:].values
+y_temp = dataset.iloc[:, 1:].values   # or [1]
 
 X_train = torch.FloatTensor(x_temp)
 Y_train = torch.FloatTensor(y_temp)
 
-#### MODEL ARCHITECTURE #### 
-
 class Model(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.linear = torch.nn.Linear(1,5)
-        self.lin2 = torch.nn.Linear(5,1)
+        self.fc1 = torch.nn.Linear(1,5)
+        self.fc2 = torch.nn.Linear(5,1)
      
     def forward(self, x):
-        x = self.linear(x)
-        y_pred = self.lin2(x)
+        x = self.fc1(x)
+        y_pred = self.fc2(x)
         return y_pred
 
 model = Model()
 
 loss_func = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-#print(len(list(model.parameters())))
-def count_params(model): 
-    return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
-### TRAINING 
 for epoch in range(500):
     X_train = Variable(X_train)
     Y_train = Variable(Y_train)
@@ -47,9 +40,7 @@ for epoch in range(500):
     optimizer.step()
     if(epoch % 50 == 0):
         print(epoch, loss)
-    #count = count_params(model)
-    #print(count)
 
 test_exp = Variable(torch.FloatTensor([[6.0]]))
-print("If u have 6 yrs exp, Salary is:", model(test_exp).data.item())
+print("6 years exp, Salary is:", model(test_exp).data.item())
 
